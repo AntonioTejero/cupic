105a0ef5f5e61c451f4701f46791e999c731cb28
Antonio Tejero <f52tecaa@uco.es>
2013-01-25 14:54:49 +0100

    Added function for reading input file

--- src/main.cu
+++ src/main.cu
@@ -31,6 +31,8 @@ struct particle
 
 /****************************** FUNCTION PROTOTIPES ******************************/
 
+void initialize (double **h_qi, double **h_qe, double **h_mi, double **h_me, double **h_kti, double **h_kte, double **h_rho, double **h_phi, double **h_Ex, double **h_Ey, particle **h_e, particle **h_i, double **d_qi, double **d_qe, double **d_mi, double **d_me, double **d_kti, double **d_kte, double **d_rho, double **d_phi, double **d_Ex, double **d_Ey, particle **d_e, particle **d_i);
+
 void read_particle_properties (double *h_qi, double *h_qe, double *h_mi, double *h_me, double *h_kti, double *h_kte);
 
 /****************************** MAIN FUNCTION ******************************/
@@ -47,12 +49,7 @@ int main (int argc, const char* argv[])
   double *d_rho, *d_phi, *d_Ex, *d_Ey;                //properties of mesh
   particle *d_e, *d_i;                                //vector of electron and ions
 
-
-  gsl_rng_env_setup();
-  static gsl_rng * rng;
-  rng = gsl_rng_alloc(gsl_rng_default);  //default random number generator (gsl)
-
-  cout << gsl_rng_uniform_pos(rng) << endl;
+  initilize (&h_qi, &h_qe, &h_mi, &h_me, &h_kti, &h_kte, &h_rho, &h_phi, &h_Ex, &h_Ey, &h_e, &h_i, &d_qi, &d_qe, &d_mi, &d_me, &d_kti, &d_kte, &d_rho, &d_phi, &d_Ex, &d_Ey, &d_e, &d_i);
 
   return 0;
 }
@@ -62,12 +59,39 @@ int main (int argc, const char* argv[])
 void initialize (double **h_qi, double **h_qe, double **h_mi, double **h_me, double **h_kti, double **h_kte, double **h_rho, double **h_phi, double **h_Ex, double **h_Ey, particle **h_e, particle **h_i, double **d_qi, double **d_qe, double **d_mi, double **d_me, double **d_kti, double **d_kte, double **d_rho, double **d_phi, double **d_Ex, double **d_Ey, particle **d_e, particle **d_i)
 {
   // allocate host memory for particle properties
-  *h_qi = double malloc(sizeof(double));
-  *h_qe = double malloc(sizeof(double));
-  *h_mi = double malloc(sizeof(double));
-  *h_me = double malloc(sizeof(double));
-  *h_kti = double malloc(sizeof(double));
-  *h_kte = double malloc(sizeof(double));
+  *h_qi = (double*) malloc(sizeof(double));
+  *h_qe = (double*) malloc(sizeof(double));
+  *h_mi = (double*) malloc(sizeof(double));
+  *h_me = (double*) malloc(sizeof(double));
+  *h_kti = (double*) malloc(sizeof(double));
+  *h_kte = (double*) malloc(sizeof(double));
+
+  // read particle properties from input file
+  read_particle_properties (*h_qi, *h_qe, *h_mi, *h_me, *h_kti, *h_kte);
+
+  // allocate device memory for particle properties
+  cudaMalloc (*d_qi, sizeof(double));
+  cudaMalloc (*d_qe, sizeof(double));
+  cudaMalloc (*d_mi, sizeof(double));
+  cudaMalloc (*d_me, sizeof(double));
+  cudaMalloc (*d_kti, sizeof(double));
+  cudaMalloc (*d_kte, sizeof(double));
+
+  // copy particle properties from host to device memory
+  cudaMemcpy (*d_qi, *h_qi, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy (*d_qe, *h_qe, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy (*d_mi, *h_mi, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy (*d_me, *h_me, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy (*d_kti, *h_kti, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy (*d_kte, *h_kte, sizeof(double), cudaMemcpyHostToDevice);
+
+  //show data
+  cout << **h_qi << endl;
+  cout << **h_qe << endl;
+  cout << **h_mi << endl;
+  cout << **h_me << endl;
+  cout << **h_kti << endl;
+  cout << **h_kte << endl;
 
   return; 
 }
