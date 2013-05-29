dd0250e3f36346c2be3dc9ed9fbe18f8f20bcb62
Antonio Tejero <f52tecaa@uco.es>
2013-05-29 10:10:32 +0200

    Added flags in main.cu for testing purpose

--- src/main.cu
+++ src/main.cu
@@ -51,24 +51,31 @@ int main (int argc, const char* argv[])
   unsigned int *d_e_bm, *d_i_bm;        // vector that stores the bookmarks (beginning and end point) for each bin (electrons and ions)
 
   initialize (&d_rho, &d_phi, &d_Ex, &d_Ey, &d_e, &d_i, &d_e_bm, &d_i_bm);
+  cout << "1" << endl;
   
   for (int i = 0; i < 10; i++, t += dt) 
   {
+    cout << "2" << endl;
     // deposit charge into the mesh nodes
     charge_deposition(d_rho, d_e, d_e_bm, d_i, d_i_bm);
     
+    cout << "3" << endl;
     // solve poisson equation
     poisson_solver(1.0e-12, d_rho, d_phi);
     
+    cout << "4" << endl;
     // derive electric fields from potential
     field_solver(d_phi, d_Ex, d_Ey);
     
+    cout << "5" << endl;
     // move particles
     particle_mover(d_e, d_e_bm, d_i, d_i_bm, d_Ex, d_Ey);
     
+    cout << "6" << endl;
     // contour condition
     cc(t, d_e_bm, &d_e, d_i_bm, &d_i, d_Ex, d_Ey);
   }
   
+  cout << "7" << endl;
   return 0;
 }
