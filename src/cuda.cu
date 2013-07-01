2a5ff7d9012957897ca26e49032fc5548f85671f
Antonio Tejero <f52tecaa@uco.es>
2013-07-01 12:29:15 +0200

    Added cuda module for cudaError handling

---
+++ src/cuda.cu
@@ -0,0 +1,42 @@
+/****************************************************************************
+ *                                                                          *
+ *    This file is part of CUPIC, a code that simulates the interaction     *
+ *    between plasma and a langmuir probe using PIC techniques accelerated  *
+ *    with the use of GPU hardware (CUDA extension of C/C++)                *
+ *                                                                          *
+ ****************************************************************************/
+
+/****************************** HEADERS ******************************/
+
+#include "diagnostic.h"
+
+/********************* HOST FUNCTION DEFINITIONS *********************/
+
+void cu_check(cudaError_t cuError) 
+{
+  // function variables
+  
+  
+  // function body
+  
+  if (0 == cuError)
+  {
+    return;
+  } else
+  {
+    cout << "CUDA error found. (error code: " << cuError << ")" << endl;
+    cout << "Exiting simulation" << endl;
+    exit(1);
+  }
+  
+}
+
+/**********************************************************/
+
+
+
+/******************** DEVICE KERNELS DEFINITIONS *********************/
+
+
+
+/**********************************************************/
---
+++ src/cuda.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+ *                                                                          *
+ *    This file is part of CUPIC, a code that simulates the interaction     *
+ *    between plasma and a langmuir probe using PIC techniques accelerated  *
+ *    with the use of GPU hardware (CUDA extension of C/C++)                *
+ *                                                                          *
+ ****************************************************************************/
+
+
+#ifndef CUDA_H
+#define CUDA_H
+
+/****************************** HEADERS ******************************/
+
+#include "stdh.h"
+
+/************************ SIMBOLIC CONSTANTS *************************/
+
+
+
+/************************ FUNCTION PROTOTIPES ************************/
+
+// host function
+void cu_check(cudaError_t cuError);
+
+
+// device kernels
+
+
+
+// device functions 
+
+
+
+#endif
