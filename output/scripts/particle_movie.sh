72819ffd8f65bcb29e10208220d3ef94279dbef3
AntonioTejero <f52tecaa@uco.es>
2014-02-18 09:32:42 +0100

    Improved performance of particle movie generation

--- output/scripts/particle_movie.sh
+++ output/scripts/particle_movie.sh
@@ -3,7 +3,7 @@
 ### USER CONFIGURATION (USER ACCESIBLE) ###
 
 PARTICLE_TIPE=electron
-INI=30000
+INI=0
 FIN=100000
 INC=1
 
@@ -12,53 +12,62 @@ INC=1
 FILENAME="$PARTICLE_TIPE"_movie 
 NUMBER_OF_BINS=100
 
-### START OF SCRIPT ###
+### GENERATION OF GNUPLOT SCRIPT ###
 
 echo set terminal jpeg size 1280,720 >> plot.gpi
 echo bin\(x,width\) = width*floor\(x/width\) >> plot.gpi
 
-for ((i=$INI, j=0; i<=$FIN; i=$i+$INC, j++))
-do
-  echo set output \""$FILENAME"_$j.jpg\" >> plot.gpi
- 
-  echo set nokey >> plot.gpi
-  echo set size 1,1 >> plot.gpi
-  echo set origin 0,0 >> plot.gpi
-  echo set multiplot >> plot.gpi
+echo j=0 >> plot.gpi
+echo do for[i=$INI:$FIN:$INC] \{ >> plot.gpi
+  
+echo set output \""$FILENAME"_\".j.\".jpg\" >> plot.gpi
+echo set nokey >> plot.gpi
+echo set size 1,1 >> plot.gpi
+echo set origin 0,0 >> plot.gpi
+echo set multiplot >> plot.gpi
+echo set tmargin 0.5 >> plot.gpi
+echo set bmargin 0.5 >> plot.gpi
+echo set rmargin 1.0 >> plot.gpi
+echo set lmargin 5.0 >> plot.gpi
+echo set grid >> plot.gpi
 
-  echo set tmargin 0.5 >> plot.gpi
-  echo set bmargin 0.5 >> plot.gpi
-  echo set rmargin 1.0 >> plot.gpi
-  echo set lmargin 5.0 >> plot.gpi
+#echo set xrange [0:12.7] >> plot.gpi
+#echo set yrange [0:102.1] >> plot.gpi
+echo set ylabel \"y\" >> plot.gpi
+echo set xlabel \"x\" >> plot.gpi
+echo set size 0.45,0.85 >> plot.gpi
+echo set origin 0.03,0.07 >> plot.gpi
+echo unset key >> plot.gpi
+echo -e set title \""$PARTICLE_TIPE" positions \(t = i\)\" >> plot.gpi
+echo -e plot \'./"$PARTICLE_TIPE"s_t_\'.i.\'.dat\' lc rgb \"blue\" >> plot.gpi
 
-#   echo set xrange [0:12.7] >> plot.gpi
-#   echo set yrange [0:102.1] >> plot.gpi
-  echo set ylabel \"y\" >> plot.gpi
-  echo set xlabel \"x\" >> plot.gpi
-  echo set grid >> plot.gpi
+#echo set xrange [0:12.7] >> plot.gpi
+#echo set yrange [0:102.1] >> plot.gpi
+echo set ylabel \"frequency \" >> plot.gpi
+echo set xlabel \"velocity\" >> plot.gpi
+echo unset ytics >> plot.gpi
+echo set size 0.45,0.85 >> plot.gpi
+echo set origin 0.51,0.07 >> plot.gpi
+echo -e set title \""$PARTICLE_TIPE" velocity distribution \(t = i\)\" >> plot.gpi
+echo -e stats \'./"$PARTICLE_TIPE"s_t_\'.i.\'.dat\' u \(sqrt\(\$3**2+\$4**2\)\) nooutput >> plot.gpi
+echo -e plot \'./"$PARTICLE_TIPE"s_t_\'.i.\'.dat\' u \(bin\(sqrt\(\$3**2+\$4**2\), \(STATS_max-STATS_min\)/$NUMBER_OF_BINS\)\):\(1.0\) smooth freq with\
+boxes lc rgb \"red\" >> plot.gpi
+echo set ytics >> plot.gpi
 
-  echo set size 0.45,0.85 >> plot.gpi
-  echo set origin 0.03,0.07 >> plot.gpi
-  echo unset key >> plot.gpi
-  echo -e set title \""$PARTICLE_TIPE" positions \(t = $i\)\" >> plot.gpi
-  echo -e plot \'./"$PARTICLE_TIPE"s_t_$i.dat\' lc rgb \"blue\" >> plot.gpi
+echo unset multiplot >> plot.gpi
+echo j=j+1 >> plot.gpi
 
-#   echo set xrange [0:12.7] >> plot.gpi
-#   echo set yrange [0:102.1] >> plot.gpi
-  echo set ylabel \"frequency \" >> plot.gpi
-  echo set xlabel \"velocity\" >> plot.gpi
-  echo unset ytics >> plot.gpi
-  echo set size 0.45,0.85 >> plot.gpi
-  echo set origin 0.51,0.07 >> plot.gpi
-  echo -e set title \""$PARTICLE_TIPE" velocity distribution \(t = $i\)\" >> plot.gpi
-  echo -e stats \'./"$PARTICLE_TIPE"s_t_$i.dat\' u \(sqrt\(\$3**2+\$4**2\)\) nooutput >> plot.gpi
-  echo -e plot \'./"$PARTICLE_TIPE"s_t_$i.dat\' u \(bin\(sqrt\(\$3**2+\$4**2\), \(STATS_max-STATS_min\)/$NUMBER_OF_BINS\)\):\(1.0\) smooth freq with\
-  boxes lc rgb \"red\" >> plot.gpi
-  echo set ytics >> plot.gpi
-  echo unset multiplot >> plot.gpi
-done
+echo \} >> plot.gpi
+
+### EXECUTE GNUPLOT SCRIPT FOR FRAMES GENERATION ###
 
 gnuplot plot.gpi
+
+### REMOVE GNUPLOT SCRIPT ###
+
 rm plot.gpi
+
+### GENERATE MOVIE FROM FRAMES AND REMOVE FRAMES ###
+
 avconv -f image2 -i "$FILENAME"_%d.jpg -b 32000k "$FILENAME".mp4
 rm *.jpg
